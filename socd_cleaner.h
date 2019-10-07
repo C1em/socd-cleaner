921c3c1b910b45ec2121e05b2ce08c30232f1cdc
martin corentin <44966090+C1em@users.noreply.github.com>
2019-10-06 21:37:13 -0700

    Create socd_cleaner.h

---
+++ socd_cleaner.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   socd_cleaner.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coremart <coremart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/06 19:49:51 by coremart          #+#    #+#             */
+/*   Updated: 2019/10/06 19:49:51 by coremart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOCD_CLEANER_H
+# define SOCD_CLEANER_H
+
+// if your keys aren't letters, you can put the code of the key decribed at :
+// https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+
+# define KEY_1 0
+# define KEY_2 1
+# define IS_DOWN 1
+# define IS_UP 0
+
+#endif
